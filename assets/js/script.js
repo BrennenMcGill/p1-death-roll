01f24253c19bda3afec582445f03e6a72e237c78
Brandon Jackson <brandonljackson23@gmail.com>
2020-06-10 07:27:12 -0600

    added Roll Dice button; results now render to the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,22 +22,43 @@
 var playerPoints = 100;
 var botPoints = 100;
 var slackname = "";
+var rollDiceBtnEl = document.querySelector("#roll-dice");
+var playerResultEl = document.querySelector("#player-roll");
+var npcResultEl = document.querySelector("#npc-roll");
+var winLoseEl = document.querySelector("#win-or-lose");
 /* ===============[ 1. Functions ]=========================*/
 
 /**
  * 1.1 function()
  */
 var rollDice = function () {
+    // generate random number between 1 and 100 for both player and npc
     var playerValue = Math.floor(Math.random() * 100) + 1;
     var npcValue = Math.floor(Math.random() * 100) + 1;
-    console.log("Player rolled: " + playerValue);
-    console.log("NPC rolled: " + npcValue);
+    //create p tag to display player's result
+    var displayPlayerValue = document.createElement("p");
+    displayPlayerValue.innerHTML = "You rolled " + playerValue;
+    //create p tag to display npc's result
+    var displayNpcValue = document.createElement("p");
+    displayNpcValue.innerHTML = "NPC rolled " + npcValue;
+    // append player's and npc's results
+    playerResultEl.appendChild(displayPlayerValue);
+    npcResultEl.appendChild(displayNpcValue);
+    // determine winner
     if(playerValue === npcValue) {
+        playerValue = "";
+        npcValue = "";
         rollDice();
     } else if (playerValue > npcValue) {
         console.log("You win!");
+        var displayWin = document.createElement("p");
+        displayWin.innerHTML = "Congrats!  You Won!";
+        winLoseEl.appendChild(displayWin);
     } else {
-        console.log("You lose!")
+        console.log("You lose!");
+        var displayLose = document.createElement("p");
+        displayLose.innerHTML = "Sorry, you lost. Try again.";
+        winLoseEl.appendChild(displayLose);
     }
 };
  /**
@@ -59,4 +80,4 @@ var rollDice = function () {
   /**
  * 2.1 Add click listeners (add, edit, delete, reset)
  */
- rollDice();
+rollDiceBtnEl.addEventListener("click", rollDice);
