8379900be3298137ac498d507389e778882a1566
Garves <garves44@gmail.com>
2020-06-11 15:20:40 -0600

    refactor javascript into Jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,87 +12,60 @@
  *   1.2 slackMessenger()
  *   1.3 srtGame()
  *   1.4 pointSystem()
- *   1.5 giphyAPI()  
+ *   1.5 giphyAPI() 
+ *   1.6 endGame()
+ *   1.7 resetGame() 
  * 
  * 2. Document Ready
  *   2.1 Add click listeners (add, edit, delete, reset)
+ *    2.1.1 $('#start-btn').on('click', function ()
+ *    2.1.2 $("#roll-dice").on("click",function)
  * 
  *********************************************************/
 
 /* ===============[ 0. GLOBALS ]=========================*/
-var slackNameEl = document.querySelector("#modalSlackName");
 var playerStatus = {
     points: 100,
     streak: 0,
     totalWins: 0
 };
 var npcPoints = 100;
-var betInputEl = document.querySelector("#bet-input");
-var playerPointsEl = document.querySelector("#playerPoints");
-var npcPointsEl = document.querySelector("#npcPoints");
-var rollDiceBtnEl = document.querySelector("#roll-dice");
-var playerResultEl = document.querySelector("#player-roll");
-var npcResultEl = document.querySelector("#npc-roll");
-var winLoseEl = document.querySelector("#win-or-lose");
-var messageEL = document.getElementById("message");
 const slackInput = JSON.parse(localStorage.getItem("slackName")) || [];
 /* ===============[ 1. Functions ]=========================*/
 /**
  * 1.1 rollDice()
  */
 var rollDice = function (bet) {
-
     // generate random number between 1 and 100 for both player and npc
     var playerValue = Math.floor(Math.random() * 100) + 1;
     var npcValue = Math.floor(Math.random() * 100) + 1;
 
-    //create p tag to display player's result
-    var displayPlayerValue = document.createElement("p");
-    displayPlayerValue.innerHTML = "You rolled " + playerValue;
+    //create p tag to display player/npc result
+    $("#player-roll").empty().append($("<p>").text(`You rolled ${playerValue}`));
+    $("#npc-roll").empty().append($("<p>").text(`NPC rolled ${npcValue}`));
 
-    //create p tag to display npc's result
-    var displayNpcValue = document.createElement("p");
-    displayNpcValue.innerHTML = "NPC rolled " + npcValue;
-
-    // append player's and npc's results
-    playerResultEl.innerHTML = "";
-    playerResultEl.appendChild(displayPlayerValue);
-    npcResultEl.innerHTML = "";
-    npcResultEl.appendChild(displayNpcValue);
 
     // DETERMINE ROUND WINNER
     if (playerValue === npcValue) {
-        playerValue = "";
-        npcValue = "";
         rollDice();
+        return;
     } else if (playerValue > npcValue) {
-        console.log("You win!");
-        var displayWin = document.createElement("p");
-        displayWin.innerHTML = "Congrats!  You Won!";
-        winLoseEl.innerHTML = "";
-        winLoseEl.appendChild(displayWin);
-        npcPoints = npcPoints - parseInt(bet);
+        $("#win-or-lose").text("").append($("<p>").text(`Congrats! You Won!`));
+        npcPoints = npcPoints - bet;
         playerStatus.points = playerStatus.points + parseInt(bet);
-        playerStatus.streak = playerStatus.streak + 1;
-        playerStatus.totalWins = playerStatus.totalWins + 1;
+        playerStatus.streak++;
+        playerStatus.totalWins++;
         pointSystem(playerStatus.points, playerStatus.streak, playerStatus.totalWins);
     } else {
-        console.log("You lose!");
-        var displayLose = document.createElement("p");
-        displayLose.innerHTML = "Sorry, you lost. Try again.";
-        winLoseEl.innerHTML = "";
-        winLoseEl.appendChild(displayLose);
+        $("#win-or-lose").text("").append($("<p>").text(`Sorry, you lost. Try again.`));
         npcPoints = npcPoints + parseInt(bet);
         playerStatus.points = playerStatus.points - bet;
         playerStatus.streak = 0;
-        playerStatus.totalWins = playerStatus.totalWins;
         pointSystem(playerStatus.points, playerStatus.streak, playerStatus.totalWins);
     }
 
     // DETERMINE GAME WINNER
-    (playerStatus.points <= 0 || npcPoints <= 0) ? ((playerStatus.points > 0) ? endGame(true) : endGame(false)) : () => {
-        return
-    };
+    (playerStatus.points <= 0 || npcPoints <= 0) ? ((playerStatus.points > 0) ? endGame(true) : endGame(false)) : () => {return};     
 };
 /**
  * 1.2 slackMessenger()
@@ -106,7 +79,6 @@ var slackMessenger = function (message, WebHook, cb, cbError) {
             console.log("SUCCESS ===", data);
         };
     }
-
     if (!WebHook) {
         $.getJSON("../../keys.json", function (responseData) {
                 return slackMessenger(message, responseData.SLACK_WEB_HOOK);
@@ -121,11 +93,11 @@ var slackMessenger = function (message, WebHook, cb, cbError) {
                 if (message.search(regex) === -1) {
                     var gif = $("<img>").addClass("").attr("src", message);
                     $("#win-or-lose").append(gif);
-                    
+
                 } else {
                     var messageHeader = $("<h3>").text(message);
                     $("#win-or-lose").append(messageHeader);
-                    
+
                 }
                 console.log("WEB HOOK NOT FOUND!");
                 return;
@@ -163,7 +135,7 @@ var slackMessenger = function (message, WebHook, cb, cbError) {
 /**
  * 1.3 srtGame()
  */
-var srtGame = function (slack) {
+var startGame = function (slack) {
     //Temporarily log slack name until start game function is done.
     console.log(slack);
 }
@@ -171,10 +143,10 @@ var srtGame = function (slack) {
  * 1.4 pointSystem()
  */
 var pointSystem = function (points, streak, wins) {
-    playerPointsEl.textContent = points;
-    npcPointsEl.textContent = npcPoints;
-    console.log(streak);
-    console.log(wins);
+    $("#playerPoints").text(points);
+    $("#npcPoints").text(npcPoints);
+    console.log("streak", streak);
+    console.log("wins", wins);
 }
 
 /**
@@ -190,17 +162,14 @@ var giphyAPI = function (result, cb, cbError) {
         };
     }
     var api_key = "sFsVukTCR6VSVrbN8OzUnJuANd3yiBET";
-
     var apiURL = `https://api.giphy.com/v1/gifs/search?q=${result}&api_key=${api_key}`;
     var randomIndex = Math.floor(Math.random() * Math.floor(24));
-    console.log("APIURL========", apiURL);
 
     fetch(apiURL)
         .then(function (response) {
             if (response.ok) {
                 response.json()
                     .then(function (gifResponse) {
-                        console.log("giphy===", gifResponse)
                         return cb(gifResponse.data[randomIndex].images.fixed_height_downsampled.url);
                     })
             } else {
@@ -221,12 +190,10 @@ var endGame = function (win_lose) {
 
     let message = `You're a ${result}!!!`;
 
-
     slackMessenger(message);
     giphyAPI(result, slackMessenger, function () {
         console.log("Error!");
     });
-
 
     // SAVE DATA TO LOCAL STORAGE
 
@@ -235,51 +202,45 @@ var endGame = function (win_lose) {
 
 };
 
+/**
+ * 1.7 resetGame()
+ */
+var resetGame = function () {
+
+};
+
 /* ===============[ 2. Document Ready ]=========================*/
 $(function () {
-
-
-
-
-
 
     /**
      * 2.1 Add click listeners (add, edit, delete, reset)
      */
-    //rollDiceBtnEl.addEventListener("click", rollDice);
-    $("#submit-message").on("click", function () {
-        endGame(false);
-    })
-
+    /**
+     * 2.1.1 $('#start-btn').on('click', function ()
+     */
     // Button to Save Slack Username to Local Storage & Start Game
     $('#start-btn').on('click', function () {
-        // Get User Input for Slack Username
-        slackName = document.querySelector("#modalSlackName").value.trim();
-        // Push Username to Array (Array gives option of pushing other things)
+        var playerName = $("#modalInputName").val().trim();
         const userArray = {
-            username: slackName
-        };
-        slackInput.push(userArray);
-        // Save User Input to Local Storage
-        localStorage.setItem("slackName", JSON.stringify(slackInput));
-        // Pass Slack Username to Start Game Function, or return error.
-        var slack = slackNameEl.value.trim();
-        if (slack) {
-            $('#user-modal').foundation('close');
-            srtGame(slack);
-            slackNameEl.value = "";
-        } else {
-            alert("Please enter a valid username.");
-        }
+                 username: playerName
+             }; 
+             (playerName) ? $('#user-modal').foundation('close') && startGame(playerName) && $("#modalInputName").val("") : $("#modal-footer").empty().append($("<p>").text("Please Enter a Username!"))
     });
 
 
-    // Button to Roll Dice
-    rollDiceBtnEl.addEventListener("click", function () {
-        event.preventDefault();
-        rollDice(betInputEl.value);
-        betInputEl.value = "";
+    /**
+     * 2.1.2 $("#roll-dice").on("click",function)
+     */
+    $("#roll-dice").on("click", () => {
+        rollDice(parseInt($("#bet-input").val()) > 0 ? $("#bet-input").val() : 0);
+        $("#bet-input").val("");
     });
 
+    /**
+     * 2.1.3 Testing
+     */
+    $("#submit-message").on("click", function () {
+        endGame(false);
+    });
 
-})
+});
